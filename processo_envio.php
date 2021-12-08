bfdb5e0f1d4a628fe22d1ed49713f5664fbce410
Antonio Netto <60482472+duzisnetto@users.noreply.github.com>
2021-12-08 16:11:23 -0300

    avançando no estudo do phpmailer

--- processo_envio.php
+++ processo_envio.php
@@ -1,6 +1,16 @@
 <?php   
+require "./bibliotecas/PHPMailer/Exception.php";
+require "./bibliotecas/PHPMailer/OAuth.php";
+require "./bibliotecas/PHPMailer/PHPMailer.php";
+require "./bibliotecas/PHPMailer/POP3.php";
+require "./bibliotecas/PHPMailer/SMTP.php";
 
-print_r($_POST);
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+
 
 class Mensagem{ 
     private $para = null;
@@ -13,7 +23,7 @@ class Mensagem{
     }
 
 
-    public function __set ($atributo, $valor){
+    public function __set($atributo, $valor){
         $this->$atributo =$valor;
         
     }
@@ -40,8 +50,41 @@ $mensagem->__set('mensagem',$_POST['mensagem']);
 
 
 
-if ($mensagem->mensagemValida()){ 
-    echo "Mensagem é válida";
-}else {
+if (!$mensagem->mensagemValida()){ 
     echo "Mensagem não é válida";
+    die();
+}try {
+        //Server settings
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'user@example.com';                     //SMTP username
+        $mail->Password   = 'secret';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    
+        //Recipients
+        $mail->setFrom('from@example.com', 'Mailer');
+        $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+        $mail->addAddress('ellen@example.com');               //Name is optional
+        $mail->addReplyTo('info@example.com', 'Information');
+        $mail->addCC('cc@example.com');
+        $mail->addBCC('bcc@example.com');
+    
+        //Attachments
+        $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+    
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Here is the subject';
+        $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    
+        $mail->send();
+        echo 'Mensagem enviada';
+    } catch (Exception $e) {
+        echo "Mensagem não enviada. ErroDB::delete('delete users where name = ?', ['John']) {$mail->ErrorInfo}";
+    }
 }
